ad1695a4efebc15a083f9abf9b420531613c31fa
JeraldAng <jeraldedricang@gmail.com>
2019-07-01 16:42:43 +0800

    update: date picker, charts, tables, options, help

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,18 +1,59 @@
-window.onload = function() {
+function mute(){
+                var music = document.getElementById("dashboard-audio"); 
+                var muter = document.getElementById("mute-button");
+                if (music.muted === false){
+                    music.muted = true;
+                    muter.value = 'Unmute Music';
+                    muter.style.backgroundColor = "#d86363";
+                    muter.onmouseover = function(){
+                        this.style.backgroundColor = "#d86363";
+                    }
+                    muter.onmouseout = function(){
+                        this.style.backgroundColor = "#dd7777";
+                    }
+                }
+                else{
+                    music.muted = false;
+                    muter.value = 'Mute Music';
+                    muter.style.backgroundColor = "#63d863";
+                    muter.onmouseover = function(){
+                    this.style.backgroundColor = "#63d863";
+                    }
+                     muter.onmouseout = function(){
+                        this.style.backgroundColor = "#77dd77";
+                    }
+                }
+}
+
+    
+    
+window.onload = function(){
+
+var ctx = document.getElementById("div_canvas");
+
+var num_charts = -1;
+var chart = [];
+var title = ""; 
+    
                 $("#file-upload-button").change(function(event){
                 var uploadedFile = event.target.files[0]; 
                 console.log(uploadedFile.type);
-                if(uploadedFile.type !== "application/json") { 
+                if(uploadedFile.type != "application/json") { 
                 alert("File not read. Please upload a .json file!"); 
                 return false;
                 }
-
+                else
+                    alert("File read successfully!"); 
+                    
                 if (uploadedFile) {
                     var readFile = new FileReader();
                     readFile.onload = function(e) { 
                         var contents = e.target.result;
                         var json = JSON.parse(contents);
-                        create(json);
+                        traverse(json,process);
+                        $('#table').show();
+                        $('.yes_data').show();
+                        $('.no_data').hide();
                     };
                     readFile.readAsText(uploadedFile);
                 } else { 
@@ -21,52 +62,221 @@ window.onload = function() {
                 });
     
                 $('#home').on('click',function(){
-                    if($('#div_canvas').css('display')!='none')
                         $('#div_canvas').show().hide();
-                    if($('#div_choose_file').css('display')!='none')
                         $('#div_choose_file').show().hide();
+                        $('#div_table').show().hide();
+                        $('#div_settings').show().hide();
+                    
+                        $('#div_home').show();
+                });
+    
+                $('#tables').on('click',function(){
+                        $('#div_canvas').show().hide();
+                        $('#div_choose_file').show().hide();
+                        $('#div_home').show().hide();
+                        $('#div_settings').show().hide();
                     
                         $('#div_table').show();
                 });
                    
                $('#charts').on('click',function(){
-                   if($('#div_table').css('display')!='none')
                         $('#div_table').show().hide();
-                   if($('#div_choose_file').css('display')!='none')
                         $('#div_choose_file').show().hide();
+                        $('#div_home').show().hide();
+                        $('#div_settings').show().hide();
                    
                         $('#div_canvas').show();
                });
                    
                 $('#upload').on('click',function(){
-                    if($('#div_table').css('display')!='none')
                         $('#div_table').show().hide();
-                    if($('#div_canvas').css('display')!='none')
                         $('#div_canvas').show().hide();
+                        $('#div_home').show().hide();
+                        $('#div_settings').show().hide();
                     
                         $('#div_choose_file').show();
                 });
     
-    }
+                $('#settings').on('click',function(){
+                        $('#div_table').show().hide();
+                        $('#div_canvas').show().hide();
+                        $('#div_home').show().hide();
+                        $('#div_choose_file').show().hide();
+                    
+                        $('#div_settings').show();
+                });
                 
-                function create(json){
-                    document.getElementById("chart_div").style.backgroundColor="white";
-                    document.getElementById("chart_div").style.border="1px rgba(0, 0, 0, 1) solid";
-                    var ctx = document.getElementById("chart_div");
-                    new Chart(ctx,
-                              {"type":"bar",
-                               "data":{"labels":["Krusty Combo","Krusty Deluxe","Krabby Pattie"],
-                                       "datasets":[{"label":"Burger Sales",
-                                                    "data":[json.burger_sales["Krusty Combo"],json.burger_sales["Krusty Deluxe"],json.burger_sales["Krabby Pattie"]],
-                                                    "fill":false,
-                                                    "backgroundColor":["rgba(236, 180, 236, 0.9)","rgba(160, 231, 160, 0.9)","rgba(255, 205, 86, 0.9)"],
-                                                    "borderColor":["rgb(195,170,195)","rgb(119,221,119)","rgb(214, 195, 45)"],
-                                                    "borderWidth":1}]},
-                               "options":{
-                                   "scales":{
-                                       "yAxes":[{
-                                           "ticks":{
-                                               "beginAtZero":true}}]}}});
+
+                function traverse(json,process) {
+                    for (var i in json) {
+                        process.apply(this,[i,json[i]]);  
+                        if (json[i] !== null && typeof(json[i])=="object") {
+                            //going one step down in the object tree!!
+                            traverse(json[i],process);
+                        }
+                    }
                 }
+
+                //called with every property and its value
+                function process(key,value) {
+                    if (value == "[object Object]"){
+                        addHeader(key); 
+                        title = key.split("_").join(" ");
+                    }
+                    else{
+                            // check to see if it is quantitative, if yes, check if no canvas was made for the category
+                            if (typeof value !== "string" && document.getElementsByClassName(title).length === 0){
+                            create_chart(title);
+                            addData(chart[num_charts], key, value);
+                            $('#table tr:last').remove();
+                            }
+                            // check quanti, if yes, check if canvas  exists
+                            else if (document.getElementsByClassName(title).length !== 0)
+                            addData(chart[num_charts], key, value);
+                            // must be quali, add to table
+                            else
+                            addContent(key, value); 
+                    }
+                }
+    
+                
+
+                function addContent(key, value) {
+                    $('#table tr:last').append('<td>' + key.split("_").join(" ") + '<\/td> <td>' + value + '<\/td>');
+                }
+    
+                function addHeader(key){
+                    $("#table").append(
+                      "<tr>" +
+                        "<th>" + key.split("_").join(" ") + ": </th>" +
+                      "</tr>"
+                  );
+                   
+                }
+    
+                function addData(chart, label, data) {
+                chart.data.labels.push(label);
+                chart.data.datasets.forEach((dataset) => {
+                    dataset.data.push(data);
+                });
+
+                chart.update();
+                }
+    
+                function create_chart(key){
+                        var new_canvas = document.createElement('canvas');
+                                                new_canvas.className = key;
+                                                ctx.appendChild(new_canvas);
+                                                new_canvas.style.backgroundColor = "white";
+                                                
+                                                num_charts++;
+                                                chart[num_charts] = new Chart(new_canvas,
+                                                      {"type":"bar",
+                                                       "data":{"labels":[],
+                                                               "datasets":[{"label":key,
+                                                                            "data":[],
+                                                                            "fill":false,
+                                                                            backgroundColor: getRandomColor(),
+                                                                            "borderWidth":1}]},
+                                                       "options":{
+                                                           responsive: false,
+                                                           maintainAspectRatio: false,
+                                                           "scales":{
+                                                               "yAxes":[{
+                                                                   "ticks":{
+                                                                       "beginAtZero":true}}]}}});  
+                                            
+                                               new_canvas.style.display = "inline-block";
+                }
+    
+                function getRandomColor() {
+                  var letters = '0123456789ABCDEF';
+                  var color = '#';
+                  for (var i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                  }
+                  return color;
+                }
+    
+                $("#chart_type").change(function () {
+                var ctype = this.value;
+                    
+                var num_of_charts = chart.length - 1;
+                while (num_of_charts >= 0){
+                        var chart_data = chart[num_of_charts].data;
+                        var chart_option = chart[num_of_charts].options;
+                        var ctx = document.getElementsByClassName(chart[num_of_charts].data.datasets[0].label);
+                    
+//                        chart[num_of_charts].clear();
+//                        chart[num_of_charts].destroy();
+                        
+                        myChart = new Chart(ctx, {
+                        type: ctype,
+                        data: chart_data,
+                        options: chart_option
+                        });
+                    
+                    num_of_charts--;
+                       
+                    }
+                });
+    
+                $("#date_picker").change(function(event){
+                    $("tr").show();
+                    
+                    var date_chosen = document.getElementById("date_picker").value;
+                    var table_data = document.getElementById("table");
+
+                    if (date_chosen !== ''){
+                        // hide all table data and headers
+                        $("td").filter(function() {
+                            return $(this).text().indexOf("") !== -1;
+                        }).parent().hide();
+                        
+                        $("th").filter(function() {
+                            return $(this).text().indexOf("") !== -1;
+                        }).parent().hide();
+                        
+                        // find table data that matches the date
+                        $("td").filter(function() {
+                            if($(this).text().indexOf(date_chosen) !== -1){
+                                return $(this).html();
+                            } 
+                        }).parent().show();
+                        
+                        
+                    }
+                                        
+                });
+                
+                $(".trigger_popup_fricc").click(function(){
+                       $('.hover_bkgr_fricc').show();
+                    });
+                    $('.hover_bkgr_fricc').click(function(){
+                        if ($('#help_page1').is(":visible")){
+                        $('#help_page1').hide();
+                        $('#help_page2').show();
+                        }
+                        else if ($('#help_page2').is(":visible")){
+                            $('.hover_bkgr_fricc').hide();
+                            $('#help_page1').show();
+                            $('#help_page2').hide();
+                        }
+                    });
+                    $('.popupCloseButton').click(function(){
+                        $('.hover_bkgr_fricc').hide();
+                        $('#help_page1').show();
+                        $('#help_page2').hide();
+                    });
+
+}
+
+
+
+
+                
+
+                
+                
                     
         
--- js/login.js
+++ js/login.js
@@ -1,18 +1,28 @@
-function validate(){
+function validate(e){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (username == "admin" && password == "p@ssword"){
-        alert ("Login successful. Welcome, Mr. Krabs!");
-        window.location = "dashboard.html"; // Redirecting to other page.
-        return false;
-    }
-    else if (username=="admin" && password != "p@assword"){
-        alert("Password incorrect. Please try again.");
-    }
-    else if(username=="" || password==""){
-        alert("Please fill in both username and password.");
-    }
-    else{
-        alert("User invalid. Please try again.");
+    
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    
+    if (keycode == '13' || keycode == '1') {
+        if (username == "admin" && password == "p@ssword"){
+            alert ("Login successful. Welcome, Mr. Krabs!");
+            window.location = "dashboard.html"; // Redirecting to other page.
+            return false;
+        }
+        else if (username=="admin" && password != "p@assword"){
+            alert("Password incorrect. Please try again.");
+        }
+        else if(username=="" || password==""){
+            alert("Please fill in both username and password.");
+        }
+        else if(username == null && password == null){
+            alert("Please enter your username and password.");
+        }
+        else{
+            alert("User invalid. Please try again.");
+        }
     }
 }
+
+
